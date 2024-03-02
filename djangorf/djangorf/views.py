8bff7a7ee144c1fe5bf2cb66ddb41b8167bb77d6
sruthijaganmohan <sruthi12jagan@gmail.com>
2024-03-02 13:37:17 +0530

    serializer get post

--- djangorf/djangorf/views.py
+++ djangorf/djangorf/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from djangorfapp.serializers import StudentSerializer
+from djangorfapp.models import Student
+
 
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
-        data = {
-            'username': 'admin',
-            'active_years': 10
-        }
-        return Response(data)
+        qs = Student.objects.all()
+        # serializer = StudentSerializer(qs, many=True)
+        student1 = qs.first()
+        serializer = StudentSerializer(student1)
+        return Response(serializer.data)
+    
+    def post(self, request, *args, **kwargs):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
